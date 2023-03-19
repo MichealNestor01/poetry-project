5eafd648e261be30667f89b42fb0b41739ada64d
Micheal Nestor <michealnestor@outlook.com>
2023-03-19 19:22:49 +0000

    Added comments and simplified the swapSentence action.

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,55 @@ import { useReducer, useEffect } from "react";
 import sentences from "./assets/sentences.json";
 import "./App.css";
 
-interface State {
+// Define the shape of the state object
+interface AppState {
   sentencesArray: string[];
   1: string;
   2: string;
   3: string;
 }
 
-type Action = {
-  type: "swapSentence";
-  sentence: 1 | 2 | 3;
-};
+// Define the possible actions that can be dispatched to the reducer
+type AppAction = { type: "swapSentence"; sentenceIndex: 1 | 2 | 3 };
 
-function reducer(state: State, action: Action): State {
+// Define the reducer function, which updates the state in response to dispatched actions
+function reducer(state: AppState, action: AppAction): AppState {
   switch (action.type) {
     case "swapSentence":
+      // Create a new array containing the current state's sentences
       const sentencesArray = [...state.sentencesArray];
+      // Pick a random index from the array
       const index = Math.floor(Math.random() * sentencesArray.length);
+      // Get the new sentence to be displayed
       const newSentence = sentencesArray[index];
+      // Remove the selected sentence from the array
       sentencesArray.splice(index, 1);
-      sentencesArray.push(state[action.sentence]);
-      switch (action.sentence) {
-        case 1:
-          return {
-            ...state,
-            sentencesArray,
-            1: newSentence,
-          };
-        case 2:
-          return {
-            ...state,
-            sentencesArray,
-            2: newSentence,
-          };
-        case 3:
-          return {
-            ...state,
-            sentencesArray,
-            3: newSentence,
-          };
-        default:
-          return state;
-      }
+      // Add the currently displayed sentence back to the end of the array
+      sentencesArray.push(state[action.sentenceIndex]);
+      // Update the state with the new sentence and shuffled array
+      return {
+        ...state,
+        sentencesArray,
+        [action.sentenceIndex]: newSentence,
+      };
     default:
       return state;
   }
 }
 
-const initialState: State = (() => {
+// Define the initial state of the app
+const initialState: AppState = (() => {
+  // Create a new array containing all the sentences
   const sentencesArray = [...sentences];
-  const randomSentences: string[] = [];
+  // Pick three random sentences to display initially
+  const randomSentences = [];
   while (randomSentences.length < 3) {
     const randomIndex = Math.floor(Math.random() * sentencesArray.length);
     const randomSentence = sentencesArray[randomIndex];
     randomSentences.push(randomSentence);
     sentencesArray.splice(randomIndex, 1);
   }
+  // Return the initial state object
   return {
     sentencesArray,
     1: randomSentences[0],
@@ -66,20 +59,22 @@ const initialState: State = (() => {
   };
 })();
 
+// Define the App component
 function App(): JSX.Element {
-  // setup state
+  // Setup state using the useReducer hook
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // Render the app UI
   return (
     <section className="pageWrapper">
       <h1>Sentences</h1>
-      <h3 className="sentence" onClick={() => dispatch({ type: "swapSentence", sentence: 1 })}>
+      <h3 className="sentence" onClick={() => dispatch({ type: "swapSentence", sentenceIndex: 1 })}>
         {state[1]}
       </h3>
-      <h3 className="sentence" onClick={() => dispatch({ type: "swapSentence", sentence: 2 })}>
+      <h3 className="sentence" onClick={() => dispatch({ type: "swapSentence", sentenceIndex: 2 })}>
         {state[2]}
       </h3>
-      <h3 className="sentence" onClick={() => dispatch({ type: "swapSentence", sentence: 3 })}>
+      <h3 className="sentence" onClick={() => dispatch({ type: "swapSentence", sentenceIndex: 3 })}>
         {state[3]}
       </h3>
     </section>
